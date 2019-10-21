00a09e828e97f4f034f33b26436c0ccd7360f362
lukas_000 <zaborowski.luk@gmail.com>
2019-10-21 08:45:05 +0200

    first commit

---
+++ PierwszappApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.zaborowski.pierwszapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PierwszappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
